a7dcfbaba976a8622e7580675de2da26420bf66e
grigorimaxim422 <grigorimaxim422@gmail.com>
2025-02-20 15:30:37 +0000

    need test

--- dataspeech/00.prepare.sh
+++ dataspeech/00.prepare.sh
@@ -1,3 +1,15 @@
 python3 prepare.py "ylacombe/jenny-tts-6h" \
   --configuration "default" \
   --model_name_or_path "google/gemma-2b-it"
+
+
+python3 main.py "ylacombe/jenny-tts-6h" \
+  --configuration "default" \
+  --text_column_name "transcription" \
+  --audio_column_name "audio" \
+  --cpu_num_workers 2 \
+  --num_workers_per_gpu_for_pitch 2 \
+  --output_dir "_cache_tags" \
+  --rename_column
+
+##Need to get audio data fc0.pt??
