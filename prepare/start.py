33d586001fcfe08b40da6d63305752d3283cc228
grigorimaxim422 <grigorimaxim422@gmail.com>
2025-02-22 17:01:12 +0000

    prepare fixing

--- prepare/start.py
+++ prepare/start.py
@@ -1,6 +1,7 @@
 from huggingface_hub import hf_hub_download
 from transformers import AutoModel, AutoTokenizer
 from huggingface_hub import snapshot_download
+from huggingface_hub import hf_hub_download
 from datasets import load_dataset, Audio
 
 DATA_CACHE_DIR="../_cache/"
@@ -36,8 +37,41 @@ download_from_hub(model_repo)
 dataset_name="ylacombe/jenny-tts-6h"
 download_dataset_from_hub(dataset_name)
 
+model_repo = "parler-tts/parler_tts_mini_v0.1"
+download_from_hub(model_repo)
 
+model_repo = "parler-tts/dac_44khZ_8kbps"
+download_from_hub(model_repo)
 
+model_repo = "google/gemma-2b-it"
+download_from_hub(model_repo)
+
+model_repo = "google/gemma-2b-it"
+download_from_hub(model_repo)
+
+DISCRIMINATOR_FILE_NAME = "discriminator_v1.0.pth"
+MODEL_PCA_FILE_NAME = "discriminator_pca_v1.0.pkl"
+    
+model_path = hf_hub_download(
+    repo_id="DippyAI-Speech/Discriminator",
+    filename=DISCRIMINATOR_FILE_NAME,  # Replace with the correct filename if different
+    cache_dir=DATA_CACHE_DIR
+)
+
+# Load the state dictionary into the model
+pca_model_path = hf_hub_download(
+    repo_id="DippyAI-Speech/PCA", filename=MODEL_PCA_FILE_NAME ,
+    cache_dir = DATA_CACHE_DIR# Replace with the correct filename if different
+)
+
+dataset_name="DippyAI/dippy_synthetic_dataset"
+download_dataset_from_hub(dataset_name)
+
+dataset_name="DippyAI/personahub_augmented_v0"
+download_dataset_from_hub(dataset_name)
+
+model_repo = "openai/whisper-tiny"
+download_from_hub(model_repo)
 
 # # Download the whole model (including config and tokenizer)
 # model_path = hf_hub_download(repo_id=model_repo)
