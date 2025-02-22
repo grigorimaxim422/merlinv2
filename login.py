7030e23f2e35d4417ebb1a6c786dedb7e44a6e51
grigorimaxim422 <grigorimaxim422@gmail.com>
2025-02-22 22:30:11 +0000

    login hf

--- login.py
+++ login.py
@@ -1,4 +1,6 @@
-from huggingface_hub import HfApi
-from huggingface_hub.commands.user import _login
+# from huggingface_hub import HfApi
+# from huggingface_hub.commands.user import _login
 
-_login(HfApi(), token="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")
+# _login(HfApi(), token="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")
+from huggingface_hub._login import _login
+_login(token='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', add_to_git_credential=False)
